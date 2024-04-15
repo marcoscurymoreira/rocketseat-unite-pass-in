b8d7b7b4c4a853e5cd251957d91e1c881dbd41a6
Marcos Moreira <marcoscurymoreira@gmail.com>
2024-04-15 16:26:33 -0300

    Aula 3 - consumindo API e trabalho com hooks

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -7,37 +7,98 @@ import { Table } from './table/table'
 import { TableHeader } from './table/table-header'
 import { TableCell } from './table/table-cell'
 import { TableRow } from './table/table-row'
-import { ChangeEvent, useState } from 'react'
-import { attendees } from '../data/attendees'
+import { ChangeEvent, useEffect, useState } from 'react'
 
 dayjs.extend(relativeTime)
 dayjs.locale('pt-br')
 
+interface Attendee {
+  id: string,
+  name: string,
+  email: string,
+  createdAt: string,
+  checkedInAt: string | null
+}
+
 export function AttendeeList() {
 
-  const [search, setSearch] = useState('')
-  const [page, setPage] = useState(1)
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString())
 
-  const totalPages = Math.ceil(attendees.length / 10)
+    if (url.searchParams.has('search')) {
+      return url.searchParams.get('search') ?? ''
+    }
+
+    return ''
+  })
+
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has('page')) {
+      return Number(url.searchParams.get('page'))
+    }
+
+    return 1
+  })
+
+  const [total, setTotal] = useState(0)
+  const [attendees, setAttendees] = useState<Attendee[]>([])
+
+  const totalPages = Math.ceil(total / 10)
+
+  useEffect(() => {
+    const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+
+    url.searchParams.set('pageIndex', String(page - 1) )
+
+    if (search.length > 0) {
+      url.searchParams.set('query', search )
+    }
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setAttendees(data.attendees)
+        setTotal(data.total)
+      })
+  }, [page, search])
+
+  function setCurrentSearch(search: string) {
+
+    const url = new URL(window.location.toString())
+    url.searchParams.set('search', search)
+    window.history.pushState({}, '', url)
+    setSearch(search)
+  }
+
+  function setCurrentPage(page: number) {
+
+    const url = new URL(window.location.toString())
+    url.searchParams.set('page', String(page))
+    window.history.pushState({}, '', url)
+    setPage(page)
+}
 
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value)
+    setCurrentSearch(event.target.value)
+    setCurrentPage(1)
   }
 
   function goToFirstPage() {
-    setPage(1)
+    setCurrentPage(1)
   }
 
   function goToPreviousPage() {
-    setPage(page - 1)
+    setCurrentPage(page - 1)
   }
 
   function goToNextPage() {
-    setPage(page + 1)
+    setCurrentPage(page + 1)
   }
 
   function goToLastPage() {
-    setPage(totalPages)
+    setCurrentPage(totalPages)
   }
 
 
@@ -47,9 +108,12 @@ export function AttendeeList() {
         <h1 className="text-2xl font-bold">Participantes</h1>
         <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
           <Search className='size-4 text-emerald-300' />
-          <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participantes..." />
+          <input 
+            onChange={onSearchInputChanged}
+            value={search}
+            className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" 
+            placeholder="Buscar participantes..." />
         </div>
-          {search}
       </div>
 
       <Table>
@@ -66,7 +130,7 @@ export function AttendeeList() {
           </tr>
         </thead>
         <tbody>
-          {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
+          {attendees.map((attendee) => {
             return (
               <TableRow key={attendee.id} className='border-b border-white/10 hover:bg-white/5'>
                 <TableCell>
@@ -76,19 +140,15 @@ export function AttendeeList() {
                 <TableCell>
                   <div className='flex flex-col gap-1'>
                     <span className='font-semibold text-zinc-50'>{attendee.name}</span>
-                    <span className='text-xs'>{attendee.email}</span>
+                    <span className='text-xs'>{attendee.email.toLowerCase()}</span>
                   </div>
                 </TableCell>
                 <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
-                <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
-                {/* CRIAR VALIDAÇÃO PARA QUE A DATA DE CHECKIN NÃO SEJA MAIOR QUE A DATA DE INSCRIÇÃO */}
-                {/* CRIAR VALIDAÇÃO PARA QUE A DATA DE CHECKIN NÃO SEJA MAIOR QUE A DATA DE INSCRIÇÃO */}
-                {/* CRIAR VALIDAÇÃO PARA QUE A DATA DE CHECKIN NÃO SEJA MAIOR QUE A DATA DE INSCRIÇÃO */}
-                {/* CRIAR VALIDAÇÃO PARA QUE A DATA DE CHECKIN NÃO SEJA MAIOR QUE A DATA DE INSCRIÇÃO */}
-                {/* CRIAR VALIDAÇÃO PARA QUE A DATA DE CHECKIN NÃO SEJA MAIOR QUE A DATA DE INSCRIÇÃO */}
-                {/* CRIAR VALIDAÇÃO PARA QUE A DATA DE CHECKIN NÃO SEJA MAIOR QUE A DATA DE INSCRIÇÃO */}
-                {/* CRIAR VALIDAÇÃO PARA QUE A DATA DE CHECKIN NÃO SEJA MAIOR QUE A DATA DE INSCRIÇÃO */}
-                {/* CRIAR VALIDAÇÃO PARA QUE A DATA DE CHECKIN NÃO SEJA MAIOR QUE A DATA DE INSCRIÇÃO */}
+                <TableCell>
+                  {attendee.checkedInAt === null
+                  ? <span className='text-zinc-400'>Não fez check-in</span>
+                  : dayjs().to(attendee.checkedInAt)}
+                </TableCell>
                 <TableCell>
                   <IconButton transparent>
                     <MoreHorizontal className='size-4'/>
@@ -101,7 +161,7 @@ export function AttendeeList() {
         <tfoot>
           <tr>
             <TableCell colSpan={3}>
-              Mostrando 10 de {attendees.length} itens
+              Mostrando {attendees.length} de {total} itens
             </TableCell>
             <TableCell className=' text-right' colSpan={3}>
               <div className='inline-flex items-center gap-8'>
